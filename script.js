b215926f83e8b382695c927e774080afabf521a4
Eduardo Pielich <107966984+Poliester2005@users.noreply.github.com>
2023-03-14 11:52:16 -0300

    primeiro programa em javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+function teste() {
+     var idade = 20;
+     var nome = "Eduardo";
+     var sobrenome = "Pielich";
+     var sexo = "Masculino";
+     var time = "São Paulo";
+     var comida = "pizza";
+     var animal = "Cachorro";
+     console.log(
+          `Meu nome é ${nome} ${sobrenome}, minha idade é ${idade} sou do sexo ${sexo} torço para o ${time} minha comida favorita é ${comida} e tenho um ${animal} como animal de estimação`
+     );
+     console.log(
+          idade +
+               " " +
+               nome +
+               " " +
+               sobrenome +
+               " " +
+               sexo +
+               " " +
+               time +
+               " " +
+               comida +
+               " " +
+               animal
+     );
+}
